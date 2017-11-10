da5276f6cc3dc0d8629232506406d7c5525bae8e
Angelo Rey Lumanta <adlumanta@up.edu.ph>
2017-11-10 17:38:26 +0800

    adjustments for stack

--- stack/LinkedList.java
+++ stack/LinkedList.java
@@ -1,8 +1,6 @@
 import java.util.NoSuchElementException;
 
 public class LinkedList<E> implements Collection<E> {
-
-	// Node
 	public class Node {
 		E data;
 		Node next;
@@ -19,9 +17,6 @@ public class LinkedList<E> implements Collection<E> {
 	LinkedList() {
 	}
 
-	// E, int -> boolean
-	// Accepts E item and an int index.
-	// Adds the item to the indicated int index.
 	public boolean add(E item, int index) {
 		Node newNode = new Node(item, head);
 
@@ -47,15 +42,20 @@ public class LinkedList<E> implements Collection<E> {
 		return true;
 	}
 
-	// int -> boolean
-	// Accepts int index as a parameter.
-	// Removes the item indicated by int index.
-	public boolean remove(int index) {
-		if (this.size == 0)
-			return false;
+	public E remove(int index) {
+		if (this.size == 0) {
+			throw new NoSuchElementException("Stack empty.");
+		}
 
 		Node current = this.head;
 		Node previous = null;
+
+		if (index == 0) {
+			previous = current;
+			current = current.next;
+			previous.next = null;
+		}
+
 		for (int i = 0; i < index; i++) {
 			previous = current;
 			current = current.next;
@@ -63,29 +63,41 @@ public class LinkedList<E> implements Collection<E> {
 				break;
 			}
 		}
+		E item = current.data;
 		previous.next = current.next;
 		current.next = null;
 		this.size--;
-		return true;
+
+		return item;
 	}
 
-	// -> int
-	// Returns the size of the list.
 	public int size() {
 		return this.size;
 	}
 
-	// -> Iterator<E>
-	// Returns an instance of the LinkedListIterator.
+	public E getData(int index) {
+		Node current = this.head;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+			if (current == null) {
+				break;
+			}
+		}
+		E item = current.data;
+		return item;
+	}
+
 	public Iterator<E> iterator() {
 		return new LinkedListIterator();
-
 	}
 
 	class LinkedListIterator implements Iterator<E> {
 		Node current = head;
 
-		@Override
+		public boolean hasNext() {
+			return (current != null);
+		}
+
 		public E next() {
 			if (!(hasNext()))
 				throw new NoSuchElementException();
@@ -94,11 +106,15 @@ public class LinkedList<E> implements Collection<E> {
 			current = current.next;
 			return item;
 		}
-
-		@Override
-		public boolean hasNext() {
-			return (current != null);
-		}
 	}
 
+	public void display() {
+		Node current = this.head;
+		int count = 0;
+		while (current != null) {
+			System.out.println("Stack" + count + ": " + current.data);
+			current = current.next;
+			count++;
+		}
+	}
 }
